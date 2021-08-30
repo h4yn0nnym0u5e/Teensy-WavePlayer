49cbdddbe128093e172d2830ccc9177630302760
Jonathan Oakley <gitJRO@0akley.co.uk>
2021-08-30 20:45:50 +0100

    First moderately OK version
    play_wav.cpp - replace wavfile with wavMovr, which does everything the file does and also wraps the buffer and EventResponder; change calculation of amount of file data to pre-load to buffer, as the old one stopped working and was hard to understand; use wavMovr to get data from buffer in update(); trigger re-load of buffer at end of update(), as if it's event-driven we need to give the foreground a chance to honour the request
    play_wav.h - implement most of WavMover class, which reads the provided file into its buffer on demand, either immediately having been called from the client's update() function, or in response to an event; move a couple of items from AudioPlayWav to WavMover

--- play_wav.cpp
+++ play_wav.cpp
@@ -49,6 +49,20 @@ static int8_t _AudioPlayWavInstance = -1;
 static uint8_t _sz_mem_additional = 1;
 #endif
 
+//----------------------------------------------------------------------------------------------------
+static bool WavMover::eventReadingEnabled = true; //!< true to read in EventResponder, otherwise reads happen under interrupt
+/*
+ * Initialise utility to move data from SD card to memory (or vice versa in the future?).
+ */
+bool WavMover::play(File file)
+{
+	wavfile = file;
+	evResp.attach(evFunc);
+	
+	return true;
+}
+//----------------------------------------------------------------------------------------------------
+
 
 FLASHMEM
 void AudioPlayWav::begin(void)
@@ -69,7 +83,7 @@ bool AudioPlayWav::play(File file, const bool paused)
 {
     stop();
 
-    wavfile = file;
+    wavMovr.play(file);
     startUsingSPI();
 
     if (!readHeader( paused ? STATE_PAUSED : STATE_PLAY ))
@@ -91,8 +105,9 @@ bool AudioPlayWav::play(const char *filename, const bool paused)
     startUsingSPI();
 
     bool irq = stopInt();
-    wavfile = SD.open(filename);
+    File file = SD.open(filename);
     startInt(irq);
+	wavMovr.play(file);
 
     if (!readHeader(paused ? STATE_PAUSED : STATE_PLAY))
     {
@@ -106,19 +121,12 @@ void AudioPlayWav::stop(void)
 {
 
     state = STATE_STOP;
-
+	SPLN("\r\nSTOP!");
     bool irq = stopInt();
-    if (wavfile) wavfile.close();
+    wavMovr.close();
     startInt(irq);
 
     stopUsingSPI();
-
-    if (buffer)
-    {
-        free(buffer);
-        buffer = nullptr;
-        sz_mem = 0;
-    }
 }
 
 
@@ -201,19 +209,15 @@ bool AudioPlayWav::readHeader(int newState)
     tDataHeader dataHeader;
     bool irq, fmtok;
 
-    if (buffer) {
-        free(buffer);
-        buffer = nullptr;
-    }
-
-    sz_mem = buffer_rd = total_length = data_length = 0;
+    buffer_rd = total_length = data_length = 0;
     channelmask = sample_rate = channels = bytes = 0;
 
     last_err = APW_ERR_FILE;
-    if (!wavfile) return false;
+    if (!wavMovr) return false;
+	
 
     irq = stopInt();
-    rd = wavfile.read(&fileHeader, sizeof(fileHeader));
+    rd = wavMovr.read(&fileHeader, sizeof(fileHeader));
     startInt(irq);
     if (rd < sizeof(fileHeader)) return false;
 
@@ -225,8 +229,8 @@ bool AudioPlayWav::readHeader(int newState)
 
     do {
         irq = stopInt();
-        wavfile.seek(position);
-        rd = wavfile.read(&dataHeader, sizeof(dataHeader));
+        wavMovr.seek(position);
+        rd = wavMovr.read(&dataHeader, sizeof(dataHeader));
         startInt(irq);
 
         if (rd < sizeof(dataHeader)) return false;
@@ -238,17 +242,17 @@ bool AudioPlayWav::readHeader(int newState)
             //Serial.println(dataHeader.chunkSize);
             irq = stopInt();
             if (dataHeader.chunkSize < 16) {
-                wavfile.read(&fmtHeader, sizeof(tFmtHeader));
+                wavMovr.read(&fmtHeader, sizeof(tFmtHeader));
                 bytes = 1;
             } else if (dataHeader.chunkSize == 16) {
-                wavfile.read(&fmtHeader, sizeof(tFmtHeaderEx));
+                wavMovr.read(&fmtHeader, sizeof(tFmtHeaderEx));
                 bytes = fmtHeader.wBitsPerSample / 8;
             } else {
                 tFmtHeaderExtensible fmtHeaderExtensible;
-                wavfile.read(&fmtHeader, sizeof(tFmtHeaderEx));
+                wavMovr.read(&fmtHeader, sizeof(tFmtHeaderEx));
                 bytes = fmtHeader.wBitsPerSample / 8;
                 memset((void*)&fmtHeaderExtensible, 0, sizeof(fmtHeaderExtensible));
-                wavfile.read(&fmtHeaderExtensible, sizeof(fmtHeaderExtensible));
+                wavMovr.read(&fmtHeaderExtensible, sizeof(fmtHeaderExtensible));
                 channelmask = fmtHeaderExtensible.dwChannelMask;
                 //Serial.printf("channel mask: 0x%x\n", channelmask);
             }
@@ -274,12 +278,12 @@ bool AudioPlayWav::readHeader(int newState)
 	data_length = dataHeader.chunkSize / (sz_frame * bytes);
 
     //calculate the needed buffer memory:
-    sz_mem = _AudioPlayWavInstances * sz_frame * bytes;
+    size_t sz_mem = _AudioPlayWavInstances * sz_frame * bytes;
     sz_mem *= _sz_mem_additional;
 
-    //allocate:
-    buffer =  (int8_t*) malloc( sz_mem );
-    if (buffer == nullptr) {
+    //allocate: note this buffer pointer is temporary
+    int8_t* buffer =  wavMovr.createBuffer( sz_mem );
+	if (buffer == nullptr) {
         sz_mem = 0;
 		last_err = APW_ERR_OUT_OF_MEMORY;
 		return false;
@@ -288,12 +292,14 @@ bool AudioPlayWav::readHeader(int newState)
     last_err = APW_ERR_OK;
 
 #if !defined(KINETISL)
+	wavMovr.setPadding((bytes == 1) ? 128:0);
     if (_AudioPlayWavInstances > 1) {
-        //For sync start, and to start immedeately:
+        //For sync start, and to start immediately:
         //The next instanceID is random. If it is not our instance, we have to fill the buffer with enough data.
 
         irq = stopInt();
-
+/*
+removed this, complex and can fail to pre-load: not sure why, can't be bothered to analyse it!
         int inst = _AudioPlayWavInstance + 1;
         if (inst >= _AudioPlayWavInstances) inst = 0;
 
@@ -304,9 +310,18 @@ bool AudioPlayWav::readHeader(int newState)
                 buffer_rd -= sz_frame * bytes;
                 if (++inst >= _AudioPlayWavInstances) inst = 0;
             } while (inst != my_instance);
-            wavfile.read(&buffer[buffer_rd], sz_mem - buffer_rd);
+            wavMovr.read(&buffer[buffer_rd], sz_mem - buffer_rd);
         }
-
+*/
+		// Simplified calculation of how much buffer to pre-load, from all down
+		// to 1/Nth depending on the instance number. This sort of works, but
+		// (a) can be sub-optimal if started in paused mode then un-paused at EXACTLY the 
+		// wrong times, (b) isn't great if additional AudioPlayWav objects are created or
+		// are deleted (which will happen with dynamic audio objects), and (c) doesn't
+		// take into account any recording objects which need SD card bandwidth and 
+		// would also need interleaving. Proper base class needed?
+		buffer_rd = my_instance*(sz_frame * bytes); // pre-load according to instance number
+        wavMovr.read(&buffer[buffer_rd], sz_mem - buffer_rd);
         state = newState;
 
         startInt(irq);
@@ -323,7 +338,8 @@ bool AudioPlayWav::readHeader(int newState)
 __attribute__((hot))
 void  AudioPlayWav::update(void)
 {
-
+    if ( state != STATE_PLAY ) return;
+/*
 #if defined(KINETISL)
     if ( state != STATE_PLAY ) return;
     if ( buffer_rd == 0)
@@ -333,7 +349,7 @@ void  AudioPlayWav::update(void)
 
     if ( state != STATE_PLAY ) return;
 
-    if (/*_AudioPlayWavInstance == my_instance &&*/ buffer_rd == 0 )
+    if (/ *_AudioPlayWavInstance == my_instance &&* / buffer_rd == 0 )
 #endif
     {
         size_t rd = wavfile.read(buffer, sz_mem);
@@ -343,8 +359,15 @@ void  AudioPlayWav::update(void)
             memset(&buffer[rd], (bytes == 1) ? 128:0 , sz_mem - rd);
         }
     }
-
+*/
     unsigned int chan;
+	int8_t* currentPos = wavMovr.getBuffer(); // buffer pointer: don't cache, could change in the future
+	size_t sz_mem = wavMovr.getBufferSize();
+	
+	if (nullptr == currentPos)
+		return;
+	else
+		currentPos += buffer_rd; // position in buffer
 
 	// allocate the audio blocks to transmit 
     audio_block_t *queue[channels];
@@ -354,7 +377,7 @@ void  AudioPlayWav::update(void)
 		if ( (queue[chan] == nullptr) ) {
 			for (unsigned int i = 0; i != chan; ++i) AudioStream::release(queue[i]);
 			last_err = APW_ERR_NO_AUDIOBLOCKS;
-			//Serial.println("Waveplayer stopped: not enough AudioMemory().");
+			SPLN("Waveplayer stopped: not enough AudioMemory().");
 			stop();
             return;
 		}
@@ -366,7 +389,7 @@ void  AudioPlayWav::update(void)
     {
 
 		// 16 bits:
-        int16_t *p = (int16_t*) &buffer[buffer_rd];
+        int16_t *p = (int16_t*) currentPos;
         buffer_rd += sz_frame * 2;
         if (buffer_rd >= sz_mem ) buffer_rd = 0;
 
@@ -392,7 +415,7 @@ void  AudioPlayWav::update(void)
     {
 
 		// 8 bits:
-		int8_t *p = &buffer[buffer_rd];
+		int8_t *p = currentPos;
 		buffer_rd += sz_frame;
         if (buffer_rd >= sz_mem ) buffer_rd = 0;
 
@@ -428,6 +451,21 @@ void  AudioPlayWav::update(void)
     //Serial.printf("%d\n",data_length);
     --data_length;
 	if (data_length <= 0) stop();
+	
+	// trigger buffer fill if we just emptied it
+#if defined(KINETISL)
+    if ( buffer_rd == 0)
+#else
+	if (++_AudioPlayWavInstance >= _AudioPlayWavInstances)
+        _AudioPlayWavInstance = 0;
+
+
+    if (/*_AudioPlayWavInstance == my_instance &&*/ buffer_rd == 0 )
+#endif
+    {
+        wavMovr.readLater();
+    }
+	
 }
 
 bool AudioPlayWav::stopInt()
@@ -589,10 +627,25 @@ uint8_t AudioPlayWav::lastErr(void)
 
 size_t AudioPlayWav::memUsed(void)
 {
-	return sz_mem;
+	return wavMovr.getBufferSize();
+}
+
+size_t AudioPlayWav::memRead(void)
+{
+	return buffer_rd;
 }
 
 uint8_t AudioPlayWav::instanceID(void)
 {
 	return my_instance;
+}
+
+uint32_t AudioPlayWav::filePos(void)
+{
+	uint32_t result = 123456L;
+	
+	if (wavMovr)
+		result = wavMovr.position();
+	
+	return result;
 }
--- play_wav.h
+++ play_wav.h
@@ -31,20 +31,152 @@
 #include <Arduino.h>
 #include <AudioStream.h>
 #include <SD.h>
+#include <EventResponder.h>
 
 
 #define APW_ERR_OK              0 // no Error
 #define APW_ERR_FORMAT          1 // not supported Format
-#define APW_ERR_FILE   			2 // File not readable (does ist exist?)
+#define APW_ERR_FILE   			2 // File not readable (does it exist?)
 #define APW_ERR_OUT_OF_MEMORY   3 // Not enough dynamic memory available
-#define APW_ERR_NO_AUDIOBLOCKS  4
+#define APW_ERR_NO_AUDIOBLOCKS  4 // insufficient available audo blocks
 
+#define xDEBUG_PRINT_PLAYWAV
+#if defined(DEBUG_PRINT_PLAYWAV)
+#define SPLN(...) Serial.println(__VA_ARGS__)
+#define SPRT(...) Serial.print(__VA_ARGS__)
+#define SPTF(...) Serial.printf(__VA_ARGS__)
+#define SFSH(...) Serial.flush()
+#else
+#define SPLN(...) 
+#define SPRT(...) 
+#define SPTF(...) 
+#define SFSH(...) 
+#endif  // defined(DEBUG_PRINT_PLAYWAV)
 
 #if defined(KINETISL)
     const int _AudioPlayWav_MaxChannels = 2;
 #else
 	const int _AudioPlayWav_MaxChannels = 16;
 #endif
+
+class AudioPlayWav;
+
+/**
+ * Class to move WAV data from (and eventually to?) files using
+ * EventResponder to decouple interrupt demand from foreground
+ * data access. Of most use with SD cards.
+ */
+class WavMover
+{
+public:
+	WavMover() {}
+	bool play(File file); //!< prepare to play a file that's already open
+	
+	// Simple functions we can define immediately:
+	void readLater(void) //!< from interrupt: request re-fill the buffer
+		{ 
+			if (eventReadingEnabled)
+				evResp.triggerEvent(0,this); // do the read in the foreground: must delay() or yield()
+			else
+				read(buffer,sz_mem);		 // read immediately
+				
+		}
+		
+	int8_t* getBuffer() //!< return pointer to buffer holding WAV data
+		{ return buffer; }
+		
+	size_t getBufferSize() //!< return size of buffer
+		{ return sz_mem; }
+		
+	int8_t* createBuffer(size_t len) //!< allocate the buffer
+		{ 
+			sz_mem = len; 
+			buffer = (int8_t*) malloc(sz_mem); 
+			SPTF("Allocated %d bytes at %X - %X\r\n",sz_mem, buffer, buffer+sz_mem-1);
+			for (size_t i=0;i<len/2;i++) *((int16_t*) buffer+i) = i * 30000 / len;
+			return buffer;
+		}
+		
+	size_t read(void* buf,size_t len) //!< read len bytes immediately into buffer provided
+		{ 
+			uint32_t tmp = ARM_DWT_CYCCNT;
+			size_t result = wavfile.read(buf,len);
+			
+			if ( result < len ) 
+				memset((int8_t*) buf+result, padding , len - result);				
+			SPTF("Read %d bytes to %x: fifth int16 is %d\r\n",len,buf,*(((int16_t*) buf)+4));
+			
+			// % CPU load per track per update cycle
+			lastReadLoad = ((ARM_DWT_CYCCNT - tmp) * AUDIO_BLOCK_SAMPLES / len)>>6;
+			
+			return result;
+		}
+		
+	void seek(size_t pos) //!< seek to new file position
+		{ wavfile.seek(pos); }
+		
+	size_t position() //!< return file position
+		{ return wavfile.position(); }
+		
+	void close() //!< close file, free up the buffer, detach responder
+		{ 
+			bool irq = stopInt();
+			
+			if (wavfile)  
+				wavfile.close();  
+			
+			if (nullptr != buffer) 
+			{ 
+				SPTF("\r\Freed %d bytes at %X - %X\r\n",sz_mem, buffer, buffer+sz_mem-1);
+				free(buffer); 
+				buffer = nullptr; 
+			}
+			
+			//evResp.detach();	
+			evResp.clearEvent(); // not intuitive, but works SO much better...
+			
+			startInt(irq);
+		}
+		
+	void setPadding(uint8_t b) { padding = b; }
+	
+	void enableEventReading(bool enable) { eventReadingEnabled = enable; }
+	
+	operator bool() {return wavfile;}
+	
+	uint32_t lastReadLoad;		//!< CPU load for last SD card read, spread over the number of audio blocks loaded
+	
+private:
+	bool stopInt()
+	{
+		if ( NVIC_IS_ENABLED(IRQ_SOFTWARE) )
+		{
+			NVIC_DISABLE_IRQ(IRQ_SOFTWARE);
+			return true;
+		}
+		return false;
+	}
+
+	void startInt(bool enabled)
+	{
+    if (enabled)
+        NVIC_ENABLE_IRQ(IRQ_SOFTWARE);
+	}	
+	
+	static void evFunc(EventResponderRef ref) //!< foreground: respond to request to load WAV data
+	{
+		WavMover& thisWM = *(WavMover*) ref.getData();
+		
+		SPRT("*** ");
+		thisWM.read(thisWM.buffer,thisWM.sz_mem);
+	}
+	EventResponder evResp; 			//!< executes data transfer in foreground
+	File wavfile;					//!< file if streaming to/from SD card
+	int8_t* buffer;					//!< buffer to store pre-loaded WAV data
+	size_t sz_mem;					//!< size of buffer	
+	uint8_t padding;				//!< value to pad buffer at EOF
+	static bool eventReadingEnabled;//!< true to read filesystem via EventResponder; otherwise inside update() as usual
+};
 
 
 class AudioPlayWav : public AudioStream
@@ -63,6 +195,7 @@ public:
 	bool isPaused(void);
 	bool isStopped(void);
 	uint32_t positionMillis(void);
+	uint32_t filePos(void);
 	uint32_t lengthMillis(void);
 	uint32_t numBits(void);
 	uint32_t numChannels(void);
@@ -70,8 +203,11 @@ public:
 	uint32_t channelMask(void);
 	uint8_t lastErr(void);              // returns last error
 	size_t memUsed(void);
+	size_t memRead(void);
 	uint8_t instanceID(void);
 	virtual void update(void);
+	void enableEventReading(bool enable) { wavMovr.enableEventReading(enable); }
+	float getCPUload() { return CYCLE_COUNTER_APPROX_PERCENT(wavMovr.lastReadLoad); }
 private:
     void begin(void);
 	bool readHeader(int newState);
@@ -79,9 +215,10 @@ private:
 	void stopUsingSPI(void);
     bool stopInt(void);
     void startInt(bool enabled);
-	File wavfile;
-	int8_t *buffer = nullptr;	        // buffer data
-	size_t sz_mem = 0;					// Size of allocated memory
+	WavMover wavMovr;
+	//File wavfile;
+	//int8_t *buffer = nullptr;	        // buffer data
+	//size_t sz_mem = 0;				// Size of allocated memory
 	size_t sz_frame;				    // Size of a sample frame in bytes
 	int data_length;		  	        // number of frames remaining in file
 	size_t buffer_rd;	                // where we're at consuming "buffer"	 Lesezeiger
