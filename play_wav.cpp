74a992d336a6a0346d792dcce908ea1a98157662
Frank Bösing <f.boesing@gmx.de>
2021-09-08 10:05:15 +0200

    update

--- play_wav.cpp
+++ play_wav.cpp
@@ -1019,7 +1019,7 @@ bool AudioRecordWav::writeHeader(File file)
 }
 
 __attribute__((hot))
-void  AudioPlayWav::update(void)
+void  AudioRecordWav::update(void)
 {
     if (state != APW_STATE_RECORD) return;
     //[...]
