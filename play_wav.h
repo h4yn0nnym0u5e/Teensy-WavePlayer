d5b401dd7253e8d9e132382902f54adf42f9bb73
Frank Bösing <f.boesing@gmx.de>
2021-09-10 21:36:56 +0200

    +example.. + bug :)

--- play_wav.h
+++ play_wav.h
@@ -155,7 +155,7 @@ private:
 	inline size_t read(void* buf,size_t len); //!< read len bytes immediately into buffer provided
 	inline size_t write(void* buf,size_t len); //!< write len bytes immediately from buffer to filesystem
 
-    void close(void); //!< close file, free up the buffer, detach responder
+    void close(bool closeFile = true); //!< close file, free up the buffer, detach responder
 
     #if USE_EVENTRESPONDER_PLAYWAV
 	static void evFuncRead(EventResponderRef ref); //!< foreground: respond to request to load WAV data
@@ -213,7 +213,7 @@ public:
 	uint32_t channelMask(void) {return channelmask;};
     uint32_t lengthMillis(void) {return total_length * (1000.0f / AUDIO_SAMPLE_RATE_EXACT);};
     #if USE_EVENTRESPONDER_PLAYWAV
-	static void enableEventReading(bool enable) { AudioBaseWav::enableEventReading(enable); }
+	static void enableEventReading(bool enable) { enableEventReading(enable); }
     #endif
 private:
     virtual void update(void);
@@ -235,7 +235,6 @@ public:
     AudioRecordWav(void): AudioStream(_AudioRecordWav_MaxChannels, queue) { begin(); }
     ~AudioRecordWav(void) { end(); }
     void stop(bool closeFile = true);
-    void pause(const bool pause);
 
     bool record(File file, APW_FORMAT fmt, unsigned int channels, bool paused = false);
     bool record(const char *filename, APW_FORMAT fmt, unsigned int channels, bool paused = false);
@@ -256,6 +255,7 @@ private:
     virtual void update(void);
     void begin(void);
     void end(void);
+    void pause(const bool pause);
     size_t (*encoder)(int8_t buffer[], size_t buffer_rd, audio_block_t *queue[], const unsigned int channels);
     audio_block_t *queue[_AudioRecordWav_MaxChannels];
     size_t sz_frame;
